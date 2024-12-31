a1b2cd5665b11ca753ecd0c92eae7144ccbee24d
Taskeren <r0yalist@outlook.com>
2024-12-31 21:51:36 +0800

    added Large Processing Factory recipe back

--- src/main/kotlin/cn/elytra/mod/gtnn/rewind/module/large_processing_factory/ModLargeProcessingFactory.kt
+++ src/main/kotlin/cn/elytra/mod/gtnn/rewind/module/large_processing_factory/ModLargeProcessingFactory.kt
@@ -2,7 +2,11 @@ package cn.elytra.mod.gtnn.rewind.module.large_processing_factory
 
 import cn.elytra.mod.gtnn.rewind.IModule
 import cn.elytra.mod.gtnn.util.copyOf
+import gregtech.api.enums.ItemList
 import gregtech.api.util.GTModHandler
+import gtPlusPlus.core.block.ModBlocks
+import gtPlusPlus.core.material.MaterialsAlloy
+import gtPlusPlus.core.util.minecraft.ItemUtils
 import gtPlusPlus.xmod.gregtech.api.enums.GregtechItemList
 import net.minecraft.item.ItemStack
 import net.minecraftforge.common.config.Configuration
@@ -45,6 +49,21 @@ object ModLargeProcessingFactory : IModule {
 			LargeProcessingFactory.copyOf(),
 			IModule.DefaultMachineRecipeMask,
 			arrayOf(GregtechItemList.Industrial_MultiMachine.get(1))
+		)
+
+		GTModHandler.addCraftingRecipe(
+			GregtechItemList.Industrial_MultiMachine.get(1), // TODO: replace this with my LPF
+			arrayOf(
+				"abc", "ded", "fgh",
+				'a', ItemList.Machine_IV_Compressor.get(1),
+				'b', ItemList.Machine_IV_Lathe.get(1),
+				'c', ItemList.Machine_IV_Polarizer.get(1),
+				'd', MaterialsAlloy.STABALLOY.getPlate(1),
+				'e', ItemUtils.getSimpleStack(ModBlocks.blockProjectTable),
+				'f', ItemList.Machine_IV_Fermenter.get(1),
+				'g', ItemList.Machine_IV_FluidExtractor.get(1),
+				'h', ItemList.Machine_IV_Extractor.get(1),
+			)
 		)
 	}
 }
