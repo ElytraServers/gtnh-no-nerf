1adb8e4c418f60cb093594081f5b2dd7a1796158
Taskeren <r0yalist@outlook.com>
2025-02-14 13:08:33 +0800

    remove available mte id output

--- src/main/java/cn/taskeren/gtnn/common/CommonProxy.java
+++ src/main/java/cn/taskeren/gtnn/common/CommonProxy.java
@@ -1,9 +1,7 @@
 package cn.taskeren.gtnn.common;
 
 import cn.elytra.mod.gtnn.GtnhNoNerf;
-import cn.taskeren.gtnn.GTNN;
 import cpw.mods.fml.common.event.*;
-import gregtech.api.GregTechAPI;
 
 public class CommonProxy {
 
@@ -21,29 +19,10 @@ public class CommonProxy {
 
 	public void onLoadComplete(FMLLoadCompleteEvent event) {
 		GtnhNoNerf.Init.fmlComplete(event);
-
-		logAvailableMachineIds();
 	}
 
 	public void onServerStarting(FMLServerStartingEvent event) {
 		GtnhNoNerf.Init.fmlServerStarting(event);
 	}
 
-	private static void logAvailableMachineIds() {
-		GTNN.logger.info("Available MTE IDs:");
-
-		var startId = 0;
-		for (int id = 0; id < GregTechAPI.METATILEENTITIES.length; id++) {
-			if (GregTechAPI.METATILEENTITIES[id] != null) {
-				if (id - 1 == startId) {
-					GTNN.logger.info("{}", id);
-				} else {
-					GTNN.logger.info("{}-{}", startId, id);
-					startId = -1;
-				}
-			} else if (startId == -1) {
-				startId = id;
-			}
-		}
-	}
 }
