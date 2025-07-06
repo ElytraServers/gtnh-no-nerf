269a2440ebf981e07f43b62d9b8df3cc495043ed
Pierre Zhang <chromasetha@outlook.com>
2025-07-07 06:48:38 +0800

    migrate to latest nightly (#26)

--- src/main/kotlin/cn/elytra/mod/gtnn/modules/large_processing_factory/LargeProcessingFactory.kt
+++ src/main/kotlin/cn/elytra/mod/gtnn/modules/large_processing_factory/LargeProcessingFactory.kt
@@ -7,7 +7,6 @@ import gregtech.api.enums.ItemList
 import gregtech.api.util.GTModHandler
 import gtPlusPlus.core.block.ModBlocks
 import gtPlusPlus.core.material.MaterialsAlloy
-import gtPlusPlus.core.util.minecraft.ItemUtils
 import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.processing.MTEIndustrialMultiMachine
 import net.minecraft.item.ItemStack
 
@@ -32,7 +31,7 @@ object LargeProcessingFactory : ModuleDefinitionBase("processing_factory") {
 				'B', ItemList.Machine_IV_Lathe.get(1),
 				'C', ItemList.Machine_IV_Polarizer.get(1),
 				'D', MaterialsAlloy.STABALLOY.getPlate(1),
-				'E', ItemUtils.getSimpleStack(ModBlocks.blockProjectTable),
+				'E', ItemStack(ModBlocks.blockProjectTable),
 				'F', ItemList.Machine_IV_Fermenter.get(1),
 				'G', ItemList.Machine_IV_FluidExtractor.get(1),
 				'H', ItemList.Machine_IV_Extractor.get(1),
